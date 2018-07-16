7d6be901897f492d59ea92268afc3c12d04f9920
Ben Watrous <ben.watrous@microsoft.com>
2018-07-16 11:01:47 -0700

    Cluster type project for IBM Spectrum Symphony

---
+++ specs/default/chef/site-cookbooks/symphony/files/default/autostop.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set PYTHON="C:\Python27\python.exe"
+set BOOTSTRAP_DIR="C:\cycle\jetpack\system\bootstrap"
+
+call "C:\Program Files\IBM\Platform Symphony\soam\7.2\w2k3_x64-vc7-psdk\bin\symenv.bat"
+
+
+%PYTHON% %BOOTSTRAP_DIR%\autostop.py > %BOOTSTRAP_DIR%\autostop.log
+ 
---
+++ specs/default/chef/site-cookbooks/symphony/libraries/helpers.rb
@@ -0,0 +1,21 @@
+module Symphony
+  class Helpers
+
+    def self.wait_for_master(sleep_time=10, max_retries=6, &block)
+      results = block.call
+      retries = 0
+      while results.length < 1 and retries < max_retries
+        sleep sleep_time
+        retries += 1
+        results = block.call
+        Chef::Log.info "Found symphony master node."
+      end
+      if retries >= max_retries
+        raise Exception, "Timed out waiting for Symphony Master"
+      end
+
+      results
+    end
+
+  end
+end
